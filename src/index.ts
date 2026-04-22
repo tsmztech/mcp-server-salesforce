7bfbaf2eb9b8f35c4356f8450615937d543588cf
rohithkumarvemulapally-stack <rohithkumar.vemulapally@gmail.com>
2026-04-22 05:38:09 +0530

    metadata js type

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ import { READ_APEX_TRIGGER, handleReadApexTrigger, ReadApexTriggerArgs } from ".
 import { WRITE_APEX_TRIGGER, handleWriteApexTrigger, WriteApexTriggerArgs } from "./tools/writeApexTrigger.js";
 import { EXECUTE_ANONYMOUS, handleExecuteAnonymous, ExecuteAnonymousArgs } from "./tools/executeAnonymous.js";
 import { MANAGE_DEBUG_LOGS, handleManageDebugLogs, ManageDebugLogsArgs } from "./tools/manageDebugLogs.js";
-import { METADATA_QUERY, handleMetadataQuery } from "./tools/metadata";
+import { METADATA_QUERY, handleMetadataQuery } from "./tools/metadata.js";
 
 // Load environment variables (using dotenv 16.x which has no stdout tips)
 // MCP servers require stdout to contain ONLY JSON-RPC messages
